a64c506aca7769a4f54771fbaa9c9a3a9895132e
ruchis25 <ruchi.shrivastava2583@gmail.com>
2017-09-21 01:48:19 -0500

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,13 @@
+
+#select data for required dates
+data<-read.csv.sql("power.txt", sep = ';', header = TRUE,sql="select * from file where Date in ('1/2/2007', '2/2/2007')")
+#combine date and time
+
+
+
+Plot1
+
+png("plot1.png", width=480, height=480)
+hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
