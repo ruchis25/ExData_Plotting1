3154788747db78962c69f7143473963622353c0f
ruchis25 <ruchi.shrivastava2583@gmail.com>
2017-09-21 01:51:41 -0500

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,4 +1,4 @@
-Plot2
+
 #select data for required dates
 data<-read.csv.sql("power.txt", sep = ';', header = TRUE,sql="select * from file where Date in ('1/2/2007', '2/2/2007')")
 #combine date and time
